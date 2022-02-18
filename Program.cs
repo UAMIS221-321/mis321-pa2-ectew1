9d0af4c5e00d77a012c9ad8686cce89ff69e22ff
Ellie Tew <ellietew@Ellies-MacBook-Air.local>
2022-02-18 16:20:59 -0600

    starting point

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mis321_pa2_ectew1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
